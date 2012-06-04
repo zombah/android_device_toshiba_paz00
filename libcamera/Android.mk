4db1653105d1c497e633f5153f2d6a7ee860dab5
zombah <zombahatgmaildotcom>
2012-06-04 14:01:58 +0400

    Replace folia version with one from Android-x86

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -1,33 +1,35 @@
-BUILD_OLD_LIBCAMERA:=
+ifeq ($(USE_CAMERA_STUB),false)
 
-ifneq ($(BUILD_TINY_ANDROID),true)
-
-LOCAL_PATH:= $(call my-dir)
-
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS:=-fno-short-enums -DHAVE_CONFIG_H 
+LOCAL_CFLAGS := -fno-short-enums -DHAVE_CONFIG_H
 
-LOCAL_C_INCLUDES += \
+LOCAL_C_INCLUDES := \
 	external/jpeg
-	
-LOCAL_SRC_FILES:= \
+
+LOCAL_SRC_FILES := \
 	CameraFactory.cpp \
 	CameraHal.cpp \
 	CameraHardware.cpp \
 	Converter.cpp \
+	SurfaceDesc.cpp \
+	SurfaceSize.cpp \
 	Utils.cpp \
 	V4L2Camera.cpp \
-	SurfaceDesc.cpp \
-	SurfaceSize.cpp 
 
-LOCAL_SHARED_LIBRARIES:= libutils libbinder libui liblog libcamera_client libcutils libmedia libandroid_runtime libhardware_legacy libc libstdc++ libm libjpeg libandroid
+LOCAL_SHARED_LIBRARIES := \
+	libcamera_client \
+	libcutils \
+	libjpeg \
+	liblog \
+	libui \
+	libutils \
 
-LOCAL_MODULE:= camera.tegra
+LOCAL_MODULE := camera.tegra
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS:= optional
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif # not BUILD_TINY_ANDROID
-
+endif
--- libcamera/CameraFactory.cpp
+++ libcamera/CameraFactory.cpp
@@ -20,11 +20,13 @@
  */
 
 #define LOG_NDEBUG 0
+#define DEFAULT_DEVICE_FRONT "/dev/video1"
+#define DEFAULT_DEVICE_BACK  "/dev/video0"
+#define CONFIG_FILE "/etc/camera.cfg"
 #define LOG_TAG "Camera_Factory"
+
 #include <cutils/log.h>
 #include <cutils/properties.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include "CameraFactory.h"
 
 extern camera_module_t HAL_MODULE_INFO_SYM;
@@ -37,18 +39,21 @@ android::CameraFactory  gCameraFactory;
 namespace android {
 
 CameraFactory::CameraFactory()
-        : mCamera(NULL)
 {
-	LOGD("CameraFactory::CameraFactory");
+    LOGD("CameraFactory::CameraFactory");
+    mCamera = NULL;
+    mCameraDevices = NULL;
+    mCameraOrientation = NULL;
+    parseConfig(CONFIG_FILE);
 }
 
 CameraFactory::~CameraFactory()
 {
-	LOGD("CameraFactory::~CameraFactory");
-    if (mCamera != NULL) {
-        delete mCamera;
-		mCamera = NULL;
+    LOGD("CameraFactory::~CameraFactory");
+    for (int i=0; i < getCameraNum(); i++) {
+        delete mCamera[i];
     }
+    free(mCamera);
 }
 
 /****************************************************************************
@@ -61,64 +66,98 @@ CameraFactory::~CameraFactory()
 
 int CameraFactory::cameraDeviceOpen(const hw_module_t* module,int camera_id, hw_device_t** device)
 {
-	LOGD("CameraFactory::cameraDeviceOpen: id = %d", camera_id);
+    LOGD("CameraFactory::cameraDeviceOpen: id = %d", camera_id);
 
-	*device = NULL;
+    *device = NULL;
 
-	if (camera_id < 0 || camera_id >= getCameraNum()) {
-		LOGE("%s: Camera id %d is out of bounds (%d)",
-			__FUNCTION__, camera_id, getCameraNum());
-		return -EINVAL;
-	}
+    if (!mCamera || camera_id < 0 || camera_id >= getCameraNum()) {
+        LOGE("%s: Camera id %d is out of bounds (%d)",
+             __FUNCTION__, camera_id, getCameraNum());
+        return -EINVAL;
+    }
 
-	/* Lets destroy the cam so we can use a new /dev/videoX */
-	if (mCamera != NULL) {
-		delete mCamera;
-		mCamera = NULL;
-	}
-
-	int handle = ::open("/dev/video1", O_RDONLY);
-	if (handle >= 0) {
-		::close(handle);
-		LOGI("Using 2 cameras!");
-		if (camera_id==0) {
-			LOGI("Returning /dev/video1");
-			mCamera = new CameraHardware(module, "/dev/video1");
-		} else {
-			LOGI("Returning /dev/video0");
-			mCamera = new CameraHardware(module, "/dev/video0");
-		}
-	} else {
-		LOGI("Using 1 camera!");
-		LOGI("Returning /dev/video0");
-		mCamera = new CameraHardware(module, "/dev/video0");
-	}
-
-	return mCamera->connectCamera(device);
+    if (!mCamera[camera_id]) {
+        mCamera[camera_id] = new CameraHardware(module, mCameraDevices[camera_id]);
+    }
+    return mCamera[camera_id]->connectCamera(device);
 }
 
 /* Returns the number of available cameras */
 int CameraFactory::getCameraNum()
 {
-	LOGD("CameraFactory::getCameraNum");
-
-	return 2;
+    LOGD("CameraFactory::getCameraNum: %d", mCameraNum);
+    return mCameraNum;
 }
-
 
 int CameraFactory::getCameraInfo(int camera_id, struct camera_info* info)
 {
-    LOGD("CameraFactory::getCameraInfo: id = %d,info = %p", camera_id,info);
+    LOGD("CameraFactory::getCameraInfo: id = %d, info = %p", camera_id, info);
 
     if (camera_id < 0 || camera_id >= getCameraNum()) {
         LOGE("%s: Camera id %d is out of bounds (%d)",
-             __FUNCTION__, camera_id, getCameraNum());
+                __FUNCTION__, camera_id, getCameraNum());
         return -EINVAL;
     }
-	
 
-	LOGD("CameraFactory::getCameraInfo: about to fetch info");
-    return CameraHardware::getCameraInfo(camera_id, info);
+    return CameraHardware::getCameraInfo(info, mCameraOrientation[camera_id]);
+}
+
+// Parse a simple configuration file
+void CameraFactory::parseConfig(const char* configFile)
+{
+    LOGD("CameraFactory::parseConfig: configFile = %s", configFile);
+
+    FILE* config = fopen(configFile, "r");
+    if (config != NULL) {
+        char line[128];
+        char arg1[128];
+        char arg2[128];
+
+        while (fgets(line, sizeof line, config) != NULL) {
+            int lineStart = strspn(line, " \t\n\v" );
+
+            if (line[lineStart] == '#')
+                continue;
+
+            sscanf(line, "%s %s", arg1, arg2);
+
+            if (strcmp(arg1, "front")) {
+                newCameraConfig(CAMERA_FACING_FRONT, arg2);
+            } else if (strcmp(arg1, "back")) {
+                newCameraConfig(CAMERA_FACING_BACK, arg2);
+            } else {
+                LOGD("CameraFactory::parseConfig: Unrecognized config line '%s'", line);
+            }
+        }
+    } else {
+        LOGD("%s not found, using camera configuration defaults", CONFIG_FILE);
+        if (access(DEFAULT_DEVICE_BACK, F_OK) != -1){
+            LOGD("Found device %s", DEFAULT_DEVICE_BACK);
+            newCameraConfig(CAMERA_FACING_BACK, DEFAULT_DEVICE_BACK);
+        }
+        if (access(DEFAULT_DEVICE_FRONT, F_OK) != -1){
+            LOGD("Found device %s", DEFAULT_DEVICE_FRONT);
+            newCameraConfig(CAMERA_FACING_FRONT, DEFAULT_DEVICE_FRONT);
+        }
+    }
+}
+
+// Although realloc could be a costly operation, we only execute this function usually 2 times
+void CameraFactory::newCameraConfig(int facing, const char* location)
+{
+    // Keep track of cameras
+    mCameraNum++;
+
+    // Grow the information arrays
+    mCamera = (CameraHardware**) realloc(mCamera, mCameraNum * sizeof(CameraHardware*));
+    mCameraDevices = (char**) realloc(mCameraDevices, mCameraNum * sizeof(char*));
+    mCameraOrientation = (int*) realloc(mCameraOrientation, mCameraNum * sizeof(int));
+
+    // Store the values for each camera_id
+    mCamera[mCameraNum - 1] = NULL;
+    mCameraDevices[mCameraNum - 1] = strdup(location);
+    mCameraOrientation[mCameraNum - 1] = facing;
+    LOGD("CameraFactory::newCameraConfig: %d -> %s", mCameraOrientation[mCameraNum - 1], mCameraDevices[mCameraNum - 1]);
 }
 
 /****************************************************************************
@@ -129,8 +168,8 @@ int CameraFactory::device_open(const hw_module_t* module,
                                        const char* name,
                                        hw_device_t** device)
 {
-	LOGD("CameraFactory::device_open: name = %s", name);
-	
+    LOGD("CameraFactory::device_open: name = %s", name);
+
     /*
      * Simply verify the parameters, and dispatch the call inside the
      * CameraFactory instance.
@@ -138,7 +177,7 @@ int CameraFactory::device_open(const hw_module_t* module,
 
     if (module != &HAL_MODULE_INFO_SYM.common) {
         LOGE("%s: Invalid module %p expected %p",
-             __FUNCTION__, module, &HAL_MODULE_INFO_SYM.common);
+                __FUNCTION__, module, &HAL_MODULE_INFO_SYM.common);
         return -EINVAL;
     }
     if (name == NULL) {
@@ -146,19 +185,20 @@ int CameraFactory::device_open(const hw_module_t* module,
         return -EINVAL;
     }
 
-    return gCameraFactory.cameraDeviceOpen(module,atoi(name), device);
+    int camera_id = atoi(name);
+    return gCameraFactory.cameraDeviceOpen(module, camera_id, device);
 }
 
 int CameraFactory::get_number_of_cameras(void)
 {
-	LOGD("CameraFactory::get_number_of_cameras");
+    LOGD("CameraFactory::get_number_of_cameras");
     return gCameraFactory.getCameraNum();
 }
 
 int CameraFactory::get_camera_info(int camera_id,
                                            struct camera_info* info)
 {
-	LOGD("CameraFactory::get_camera_info");
+    LOGD("CameraFactory::get_camera_info");
     return gCameraFactory.getCameraInfo(camera_id, info);
 }
 
