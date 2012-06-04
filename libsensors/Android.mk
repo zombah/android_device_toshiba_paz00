6b70d6cf690564a2d4a3a8e2b58d760b2f333721
zombah <zombahatgmaildotcom>
2012-06-04 14:04:32 +0400

    Kbdsensor from Android-x86

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -1,20 +1,30 @@
+# Copyright (C) 2011 The Android-x86 Open Source Project
+
 LOCAL_PATH := $(call my-dir)
 
+# HAL module implemenation, not prelinked and stored in
+# hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
-
-# Include kerne headers in a proper way later on
-#LOCAL_C_INCLUDES:= $(LOCAL_PATH)/../../../../kernel/include
-
-LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_MODULE := sensors.tegra
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PRELINK_MODULE := false
+ifeq ($(strip $(BOARD_USES_KBDSENSOR)),true)
+LOCAL_SRC_FILES := kbdsensor.cpp
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-# include any shared library dependencies
-
-LOCAL_SRC_FILES := sensors.c
+ifeq ($(strip $(BOARD_USES_KBDSENSOR_ROTKEY1)),true)
+LOCAL_CFLAGS := -DFN_ROT_0=KEY_F9 -DFN_ROT_90=KEY_F12 -DFN_ROT_180=KEY_F10 -DFN_ROT_270=KEY_F11
+endif
+ifeq ($(filter -DFN_ROT_0=%,$(LOCAL_CFLAGS)),)
+LOCAL_CFLAGS := -DFN_ROT_0=KEY_UP -DFN_ROT_90=KEY_RIGHT -DFN_ROT_180=KEY_DOWN -DFN_ROT_270=KEY_LEFT
+endif
 
 include $(BUILD_SHARED_LIBRARY)
+endif
+
+ifeq ($(strip $(BOARD_USES_HDAPS_ACCEL)),true)
+LOCAL_SRC_FILES := hdaps.c
+include $(BUILD_SHARED_LIBRARY)
+endif
